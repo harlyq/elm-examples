4b170c4238904adf9a13e21ddf3b3391a5c7c08a
harlyq <celerity252@gmail.com>
2015-07-26 14:15:24 -0400

    animation and debuggin

--- mancala.elm
+++ mancala.elm
@@ -8,6 +8,7 @@ import Mouse exposing (..)
 import Time exposing (..)
 import Signal exposing (..)
 import Mouse exposing (..)
+import Debug exposing (..)
 
 colA = Color.blue
 colB = Color.purple
@@ -23,7 +24,7 @@ totalPits = 2 + numPits*2
 
 lerp : Float -> Float -> Time -> Time -> Float
 lerp from to duration t =
-  from + (to - from)*(t/duration)
+  if t < duration then from + (to - from)*(t/duration) else to
   
 toCartesian : (Int, Int) -> (Float, Float)
 toCartesian (x,y) = ((toFloat x)/boardWidth*2 - 1, 1 - (toFloat y)/boardHeight*2)
@@ -91,6 +92,29 @@ drawBoard : Board -> Element
 drawBoard board =
   collage boardWidth boardHeight <| List.concatMap (drawPit board) <| [0..(2*numPits + 1)]
 
+type AnimationStyle = Scale | PosY
+type alias Animation = { style: AnimationStyle, tween: Float -> Float, i: Int, t: Float }
+
+tickAnimation : Time -> Animation -> Animation
+tickAnimation ms animation = { animation | t <- animation.t + (inSeconds ms) }
+
+tickAnimationList : Time -> List Animation -> List Animation
+tickAnimationList ms animations = List.map (tickAnimation ms) animations
+
+tickBoard : Board -> Animation -> Board
+tickBoard board animation =
+  let
+    i = animation.i
+    v = animation.tween animation.t
+  in
+    case animation.style of
+      Scale -> { board | scales <- set i v board.scales }
+      PosY -> { board | scales <- set i v board.posYs }
+      
+tickBoardAnimations : List Animation -> Board -> Board
+tickBoardAnimations animations board =
+  List.foldl (\anim board -> tickBoard board anim) board animations
+
 animateBoard : Time -> Board -> Board
 animateBoard ms board =
   let
@@ -98,13 +122,26 @@ animateBoard ms board =
     v = withDefault 1 <| get i board.scales
   in
     { board | scales <- set i (v + 0.01) board.scales }
-    
-layer1 = drawBoard gameBoard
-layer2 = Signal.map (show << getPitFromMouse gameBoard) Mouse.position
+
+animationList : List Animation
+animationList = [{style = Scale, tween = lerp 1 2 0.5, i = 0, t = 0}]
+
+isActive animation = animation.t < 10
+
+layer1 = Signal.map drawBoard gameBoard'
+layer2 = Signal.map2 (\board pos -> show <| getPitFromMouse board pos) gameBoard' Mouse.position
+deltaTime = fps 30
+gameBoard' = Signal.foldp tickBoardAnimations gameBoard animationList3'
+animationList' = Signal.foldp tickAnimationList animationList deltaTime
+animationList2' = Signal.map (\animList -> List.filter isActive animList) animationList'
+
+animationList3' = Debug.watch "animationList" <~ animationList2'
+
+--gameMerge = Signal.map2 (\animationList dt -> Signal.foldp (tickBoardAnimations animationList) gameBoard dt) animationList' deltaTime
   
 main : Signal Element
 main =
-  Signal.map (above layer1) layer2
+  Signal.map2 above layer1 layer2
   --show gameBoard.posXs
   --collage boardWidth boardHeight <| drawPit gameBoard 8
   --show <| fromList <| List.reverse <| toList pitXs
